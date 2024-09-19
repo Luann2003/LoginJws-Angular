a1100994721aafaabe0be579d4fbb3d2a1a1c389
Luann2003 <lvictorluciano@outlook.com>
2024-09-19 11:01:00 -0300

    completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {  provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import {provideAnimations} from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideToastr(),
+    provideAnimations(),
     provideHttpClient(withFetch()),
     provideRouter(routes)]
 };
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 interface LoginForm{
   username: FormControl,
@@ -22,19 +24,34 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private loginService: LoginService) {
+    private loginService: LoginService,
+    private toastService: ToastrService) {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     })
   }
 
-  submit(){
+  submit() {
     this.loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
-      next: () => this.router.navigate(['/user']),
-      error: () => console.log("Erro inesperado! Tente novamente mais tarde")
-    })
-   
-  }
+      next: () => {
+        this.toastService.success("Login realizado com sucesso!"); 
+        this.router.navigate(['/user']); 
+      },
+      error: (err) => {
+        if (err.status === 400) {
+          this.toastService.error("Usuário ou senha incorretos.");
+        }else if (err.status === 401) {
+          this.toastService.error("Credenciais invalida");
+        } 
+        else if (err.status === 500) {
+          this.toastService.error("Erro no servidor. Tente novamente mais tarde.");
 
+        } else {
+          this.toastService.error("Erro inesperado! Tente novamente.");
+        }
+      }
+    });
+    this.loginForm.reset();
+  }
 }
